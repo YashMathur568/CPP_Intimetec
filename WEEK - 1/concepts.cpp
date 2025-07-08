447a01ab2a4121813d44aaea1853f59bcf57c852
YashMathur568 <yash.mathur@intimetec.com>
2025-07-08 08:38:37 +0530

    List Initialization

--- WEEK - 1/concepts.cpp
+++ WEEK - 1/concepts.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
-// using namespace std;
+using namespace std;
 
 //Nested Function Definations are Illegal in c++
+
 // int main() {
 //     void print(int x) {
 //         cout << x;
@@ -9,10 +10,55 @@
 //     return 0;
 // }
 
-void print(int x) {
-    std::cout << x;
-}
+//Implicit Typecasting
 
-int main() {
-    print(3.14);
+// void print(int x) {
+//     std::cout << x;
+// }
+// int main() {
+//     print(3.14);
+// }
+
+//Types of namespaces
+
+// 1. Names namespace
+// namespace MyNamespace {
+//     int value = 10;
+// }
+
+// 2. Unnamed namespace
+//Only accessible in that cpp file
+// namespace {
+//     int hidden = 42;  // Only accessible in this translation unit
+// }
+
+// 3. Nested Namespace
+// namespace A {
+//     namespace B {
+//         void greet() {}
+//     }
+// }
+//OR
+// namespace A::B {
+//     void greet() {}
+// }
+// Usage-> A::B::greet();
+
+// 4. Inline Namespace
+// Introduced in C++11 to support versioning. Members of an inline namespace are treated as if they belong to the outer namespace.
+// namespace MyLib {
+//     inline namespace v1 {
+//         void doSomething();
+//     }
+//     namespace v2 {
+//         void doSomething();
+//     }
+// }
+// MyLib::doSomething();  // Calls v1 version by default
+// MyLib::v2::doSomething();  // Explicit version
+
+int main(){
+    int x {};      // value initialization (List Initialization)
+    cout<<x;
+    return 0;
 }
