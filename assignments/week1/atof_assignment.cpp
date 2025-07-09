f6e1e46284a59066f3faf4a39be917d5dcd178d3
YashMathur568 <yash.mathur@intimetec.com>
2025-07-09 17:19:42 +0530

    Handles scientific Notation

--- assignments/week1/atof_assignment.cpp
+++ assignments/week1/atof_assignment.cpp
@@ -2,6 +2,79 @@
 #include <string>
 #include <cmath>
 #include <iomanip>
+
+void handleEValue(std::string str)
+{
+    int i = 0;
+    while (str[i] == ' ' && str[i] != '\0')
+    {
+        i++;
+    }
+    double mantissa = 0;
+    double exponent = 0;
+    int mlength = 0;
+    bool eAppeared = false;
+    bool negativeExponent = false;
+    int sign = 1;
+    if (str[i] == '-')
+    {
+        sign = -1;
+        i++;
+    }
+    while (str[i] != '\0')
+    {
+        if (str[i] >= '0' && str[i] <= '9')
+        {
+            if (eAppeared == false)
+            {
+                mantissa = mantissa * 10 + (str[i] - '0');
+                mlength++;
+                i++;
+            }
+            else
+            {
+                exponent = exponent * 10 + (str[i] - '0');
+                i++;
+            }
+        }
+        else if (str[i] == 'e')
+        {
+            if (mantissa == 0)
+            {
+                break;
+            }
+            else
+            {
+                if (eAppeared == false)
+                {
+                    i++;
+                    eAppeared = true;
+                    if (str[i] == '-')
+                    {
+                        negativeExponent == true;
+                        i++;
+                    }
+                }
+                else
+                {
+
+                    break;
+                }
+            }
+        }
+    }
+    mantissa = mantissa / pow(10, (mlength - 1));
+
+    if (negativeExponent == false)
+    {
+        std::cout << sign * mantissa << "e+" << exponent + mlength - 1;
+    }
+    else
+    {
+        std::cout << sign * mantissa << "e+" << exponent - mlength + 1;
+    }
+}
+
 double my_atof(std::string str)
 {
     int i = 0;
@@ -24,6 +97,11 @@ double my_atof(std::string str)
 
     while (str[i] != '\0')
     {
+        if (str[i] == 'e')
+        {
+            handleEValue(str);
+            exit(1);
+        }
         if (str[i] >= '0' && str[i] <= '9')
         {
             if (decimalAppeared == false)
