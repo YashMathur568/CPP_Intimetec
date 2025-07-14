4c23fa262aa31e638ae84532fbe503774e19a9aa
YashMathur568 <yash.mathur@intimetec.com>
2025-07-14 08:58:10 +0530

    UUID code

---
+++ WEEK - 1/10-07-2025/UUID.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+#pragma pack(1)
+struct IDs
+{
+    unsigned short manufactureId;
+    unsigned int productId;
+};
+#pragma pack()
+
+union UUID
+{
+    unsigned char byteArray[6];
+    IDs id;
+};
+
+int main()
+{
+
+    UUID dId;
+
+    dId.id.manufactureId = 1001;
+    dId.id.productId = 23456;
+
+    std::cout << "Size of struct: " << sizeof(dId.id) << std::endl;
+
+    for (int i = 0; i < 6; i++)
+    {
+        std::cout << (int)dId.byteArray[i] << std::endl;
+    }
+}
