895080eb99f4e5b97f47e0fd312ba5ef0be0c685
YashMathur568 <yash.mathur@intimetec.com>
2025-07-17 08:47:22 +0530

    What happens if no #endif is used

---
+++ WEEK - 2/17-07-2025/endif.cpp
@@ -0,0 +1,7 @@
+// #include <iostream>
+// #define DEBUG
+// #ifdef DEBUG
+// std::cout << "Debug line 1" << endl;
+// std::cout << "Debug line 2" << endl;
+
+// cout << "This line - debug or not??" << endl;
