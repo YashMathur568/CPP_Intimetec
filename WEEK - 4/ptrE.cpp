7ee9e733be4e2cd96218c4420eb5047dee8210e9
YashMathur568 <yash.mathur@intimetec.com>
2025-07-28 11:37:56 +0530

    Exploring Different Types Of Errors

---
+++ WEEK - 4/ptrE.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+// int *getDanglingPointer()
+// {
+//     int a = 22;
+//     return &a;
+// }
+
+int b;
+int arr1[4];
+
+int main()
+{
+    // int *ptr = new int(1);
+    // delete ptr;
+    // delete ptr;
+
+    // int *p = getDanglingPointer();
+    // std::cout << *p << std::endl;
+
+    // int *ptr = nullptr;
+    // *ptr = 5;
+
+    // int a = 10;
+    // int arr[4] = {1, 2, 3, 4};
+
+    // int *ptr = nullptr;
+    // cout << ptr << endl;
+
+    // int x = NULL;
+    // cout << x << endl;
+    // int y = nullptr;
+    // cout << y << endl;
+
+    return 0;
+}
